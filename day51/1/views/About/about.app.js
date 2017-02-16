ca437d160a05635eaf9f81419dffe9efce1e1f14
Vschool <vschool@ali.Makassed.net>
2017-02-16 21:07:07 +0200

    16/2

--- day51/1/views/About/about.app.js
+++ day51/1/views/About/about.app.js
@@ -2,10 +2,14 @@ var app = angular.module("app.about", ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider.when("/about", {
-    templateUrl: "./view/About/about.app.js",
+    templateUrl: "./views/About/about.app.html",
     controller: "aboutCtrl"
   });
 });
 
 app.controller("aboutCtrl", function($scope) {
+    $scope.submitted = false;
+    $scope.showinfo=function(){
+        $scope.submitted = true;
+    }
 });
